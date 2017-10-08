7b24ab56529bd4fcafeaed73efca64c949b5db4c
Alexander Ivankov <AlexanderIvankoff@gmail.com>
2017-10-08 21:24:54 +0300

    @angular/material bug: off ('noUnusedParameters') in tsconfig

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MdInputModule, MdToolbarModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MdToolbarModule,
+    MdInputModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
